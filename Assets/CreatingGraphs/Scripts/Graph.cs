2731e9560f41a3e969fabfc78c0a4b5850bc8a1a
Mariohuc <mhuaypuna@unsa.edu.pe>
2018-10-09 19:28:47 -0500

    First commit

---
+++ Assets/CreatingGraphs/Scripts/Graph.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph : MonoBehaviour {
+
+	// Use this for initialization
+	public Transform pointPrefab;
+
+	void Awake()
+	{
+		for(int i=0;i<10;i++){
+			Transform point = Instantiate(pointPrefab);
+			point.localPosition = Vector3.right * i;
+		}
+		
+
+	}
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

9e4fcd49d822da6e60082cdb22fad617857422ff
Mariohuc <mhuaypuna@unsa.edu.pe>
2018-10-10 19:17:48 -0500

    Second commit

--- Assets/CreatingGraphs/Scripts/Graph.cs
+++ Assets/CreatingGraphs/Scripts/Graph.cs
@@ -6,12 +6,24 @@ public class Graph : MonoBehaviour {
 
 	// Use this for initialization
 	public Transform pointPrefab;
+	//resolution
+	[Range (10,100)] public int resolution = 10;
 
 	void Awake()
 	{
-		for(int i=0;i<10;i++){
+		float step = 2f / resolution;
+		Vector3 scale = Vector3.one * step; // 2/10 = 1/5 cada cubo mide 0.2 pero la posicion central va al medio
+		Vector3 position; // x ya no porque es definida en el loop
+						  //position.y = 0f;
+		position.z = 0f;
+		for (int i=0;i < resolution; i++){
 			Transform point = Instantiate(pointPrefab);
-			point.localPosition = Vector3.right * i;
+			position.x = ((i + 0.5f) * step - 1f);
+			//position.y = position.x; // function y = f(x) => f(x) = x
+			position.y = position.x * position.x; // function y = f(x) => f(x) = x^2
+			point.localPosition = position;
+			point.localScale = scale;
+			point.SetParent(transform,false);
 		}
 		
 

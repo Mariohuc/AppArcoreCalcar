44c65592ef2707b5586d40c21c2c4688d1c027d7
Mariohuc <mhuaypuna@unsa.edu.pe>
2018-10-11 19:55:28 -0500

    Create animated graphics to scene

--- Assets/CreatingGraphs/Scripts/Graph.cs
+++ Assets/CreatingGraphs/Scripts/Graph.cs
@@ -8,32 +8,85 @@ public class Graph : MonoBehaviour {
 	public Transform pointPrefab;
 	//resolution
 	[Range (10,100)] public int resolution = 10;
+	private Transform[] points = null;
 
 	void Awake()
 	{
+		bool conAnimacion = true;
+
 		float step = 2f / resolution;
 		Vector3 scale = Vector3.one * step; // 2/10 = 1/5 cada cubo mide 0.2 pero la posicion central va al medio
-		Vector3 position; // x ya no porque es definida en el loop
+		Vector3 position; // coordanda x ya no porque es definida en el loop
 						  //position.y = 0f;
+		position.y = 0f;
 		position.z = 0f;
-		for (int i=0;i < resolution; i++){
-			Transform point = Instantiate(pointPrefab);
-			position.x = ((i + 0.5f) * step - 1f);
-			//position.y = position.x; // function y = f(x) => f(x) = x
-			position.y = position.x * position.x; // function y = f(x) => f(x) = x^2
-			point.localPosition = position;
-			point.localScale = scale;
-			point.SetParent(transform,false);
-		}
 		
+		if (conAnimacion)
+		{
+			
+			points = new Transform[resolution];
+			for (int i = 0; i < points.Length; i++)
+			{
+				Transform point = Instantiate(pointPrefab);
+				position.x = ((i + 0.5f) * step - 1f);
+				//position.y = position.x; // function y = f(x) => f(x) = x
+				//position.y = position.x * position.x; // function y = f(x) => f(x) = x^2
+				//position.y = position.x * position.x * position.x; // function y = f(x) => f(x) = x^3
+				point.localPosition = position;
+				point.localScale = scale;
+				point.SetParent(transform, false);
+				points[i] = point;
+			}
+
+		}
+		else {
+
+			for (int i = 0; i < resolution; i++)
+			{
+				Transform point = Instantiate(pointPrefab);
+				position.x = ((i + 0.5f) * step - 1f);
+				//position.y = position.x; // function y = f(x) => f(x) = x
+				//position.y = position.x * position.x; // function y = f(x) => f(x) = x^2
+				position.y = position.x * position.x * position.x; // function y = f(x) => f(x) = x^3
+				point.localPosition = position;
+				point.localScale = scale;
+				point.SetParent(transform, false);
+			}
+		}
+
 
 	}
+
+
+
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		float t = Time.time;
+		for(int i = 0 ; i < points.Length; i++) {
+			Transform point = points[i];
+			Vector3 position = point.localPosition;
+			//position.y = position.x * position.x;
+			//position.y = SineFunction( position.x , t);
+			position.y = MultiSineFunction(position.x, t);
+			point.localPosition = position;
+		}
 	}
+
+	static float SineFunction(float x, float t) {
+		return Mathf.Sin(Mathf.PI * (x + t));
+	}
+
+	static float MultiSineFunction(float x, float t)
+	{
+		float y = Mathf.Sin(Mathf.PI * (x + t));
+		y += Mathf.Sin(2f * Mathf.PI * (x + t)) / 2f;
+		y *= 2f / 3f; // I don't understand whyy but I think this's for the function range.
+		return y;
+
+	}
+	
 }
